6659b18907d49bb853954824b3a0f06e4060faad
wangwenli1991 <1527179450@qq.com>
2018-11-03 15:24:37 +0800

    '1'

--- database/migrations/2017_02_04_075515_create_attachments_table.php
+++ database/migrations/2017_02_04_075515_create_attachments_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateAttachmentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('attachments', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('avatar');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('path')->nullable();
+            $table->integer('user_id')->default('0');
+            $table->string('hash1')->nullable();
+            $table->string('md5')->nullable();
+            $table->string('ipaddress')->default('');
             $table->timestamps();
         });
     }
@@ -31,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('attachments');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,21 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['namespace' => 'App'], function () {
+    Route::get('/', 'HomeController@index')->name('home');
+    Route::get('/about', 'About@index')->name('home');
+    Route::get('/service', 'Service@index')->name('home');
+    Route::get('/portfolio', 'Portfolio@index')->name('home');
+    Route::get('/pages', 'Pages@index')->name('home');
+    Route::get('/blog', 'Blog@index')->name('home');
+    Route::get('/contact', 'Contact@index')->name('home');
+    // Route::get('/post/{flag}', 'HomeController@posts')->name('post');
+    // Route::get('/tags/{flag}', 'HomeController@tags')->name('tags');
+    // Route::get('/category/{flag}', 'HomeController@category')->name('category');
+    // Route::get('/feed', 'HomeController@feed');
+    // Route::get('/sitemap.xml', 'HomeController@siteMap');
+    // Route::get('/xmlrpc', 'XmlRpcController@errorMessage');
+    // Route::post('/xmlrpc', 'XmlRpcController@index')->name('xmlrpc');
+    // Route::get('/friends', 'HomeController@friends')->name('friends');  
+    // Route::resource('/comment', 'CommentController');
+    // Route::get('/debug', 'HomeController@debug')->name('debug');
 });
